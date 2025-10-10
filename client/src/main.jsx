85e302cfb72802e969a53ba438f899cadc5a61fb
Jiril Zala <zjiril@gmail.com>
2025-10-10 14:16:01 -0400

    SCRUM-176 Frontend: Create login form UI in React

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './styles/auth.css'
 import AuthLayout from './components/AuthLayout'
+import Login from './pages/auth/Login'
 import Signup from './pages/auth/Signup'
 
 function App(){
@@ -11,8 +12,11 @@ function App(){
       <Routes>
         <Route path="/auth" element={<AuthLayout/>}>
           <Route index element={<Navigate to="login" replace />} />
+          <Route path="login" element={<Login/>} />
           <Route path="signup" element={<Signup/>} />
+          <Route path="forgot" element={<Forgot/>} />
         </Route>
+        <Route path="/" element={<Navigate to="/auth/login" replace />} />
       </Routes>
     </BrowserRouter>
   )
