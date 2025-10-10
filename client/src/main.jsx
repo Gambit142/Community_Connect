da8d8681c16f84356dca498bfa04125cfe0bd3d9
Jiril Zala <zjiril@gmail.com>
2025-10-10 13:10:15 -0400

    SCRUM-171 Frontend: Create registration form UI in React

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './styles/auth.css'
+import AuthLayout from './components/AuthLayout'
+import Login from './pages/auth/Login'
+import Signup from './pages/auth/Signup'
+import Forgot from './pages/auth/Forgot'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout/>}>
+          <Route index element={<Navigate to="login" replace />} />
+          <Route path="signup" element={<Signup/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<App />)
